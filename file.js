6aab101279f577af204205e72c77339431510ae2
lcy <454036111@qq.com>
2017-11-28 15:12:52 +0800

    study..

--- file.js
+++ file.js
@@ -1,13 +1,15 @@
 "use strict";
-var Hello = /** @class */ (function () {
-    function Hello(fiestName, lastName) {
-        this.firstName = fiestName;
-        this.lastName = lastName;
+console.log(greetNamed("张三"));
+function greetNamed(name) {
+    if (name) {
+        return " Hi! " + name;
     }
-    Hello.prototype.greeter = function () {
-        return "欢迎来到typescript的世界，hello" + this.firstName + " " + this.lastName;
-    };
-    return Hello;
-}());
-var user = new Hello("王", "小二");
-document.body.innerHTML = user.greeter();
+    return "empty";
+}
+var greetUnnamed = function (name) {
+    if (name) {
+        return "Hi! " + name;
+    }
+    return "empty";
+};
+console.log(greetUnnamed("李四"));
--- file.ts
+++ file.ts
@@ -1,15 +1,44 @@
-class Hello {
-    firstName : string;
-    lastName : string;
-    constructor(fiestName : string,  lastName : string) {
-        this.firstName = fiestName;
-        this.lastName = lastName;
+console.log(greetNamed("张三"))
+
+
+function greetNamed(name : string): string {
+    if (name) {
+        return " Hi! " + name;
     }
-    greeter() {
-        return "欢迎来到typescript的世界，hello" + this.firstName + " " + this.lastName;
-    }
+    return "empty";
 }
 
-var user = new Hello("王", "小二");
+var greetUnnamed = function (name: string) : string {
+    if (name) {
+        return "Hi! " + name;
+    }
+    return "empty";
+}
+console.log(greetUnnamed("李四"));
 
-document.body.innerHTML = user.greeter();
+var greetName2 : (name: string) => string;
+greetName2 = function (name: string): string {
+    return "this is greetName2"
+}
+
+//与可选参数的函数
+function add(foo: number, bar: number, foobar: number): number {
+    return foo + bar + foobar;
+}
+
+function add_optionals (foo: number, bar: number, foobar?: number): number {
+    var result = foo + bar;
+    if (foobar !== undefined) {
+        result += foobar;
+    }
+    return result;
+}
+
+//有默认参数的函数
+function add_defaultNumber (foo: number, bar?: number, foobar: number = 0): number {
+    return foo + foobar;
+}
+
+//有剩余参数的函数
+
+
